258069d4e31f2b12ae20795ac3f79568a73ab7ba
Devendra Singh Shekhawat <dev.shekhawat.dev@gmail.com>
2017-04-28 18:35:26 +0530

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -29,19 +29,19 @@ $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $response = null;
 
 
-$ch1 = curl_init();
-curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch1, CURLOPT_URL, 'https://graph.facebook.com/v2.6/'.$senderId.'?fields=first_name,last_name&access_token='.$accessToken);
-$result1 = curl_exec($ch1);
-curl_close($ch1);
-$obj = json_decode($result1);
+//$ch1 = curl_init();
+//curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+//curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch1, CURLOPT_URL, 'https://graph.facebook.com/v2.6/'.$senderId.'?fields=first_name,last_name&access_token='.$accessToken);
+//$result1 = curl_exec($ch1);
+//curl_close($ch1);
+//$obj = json_decode($result1);
 
 //set Message
 
 if($messageText == "hi" || $messageText == "Hi") {
 
-$answer = "Hello".$obj['first_name'];
+$answer = "Hello";
 
 }
 else{
