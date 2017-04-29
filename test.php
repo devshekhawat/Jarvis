da477a4080edfaea1f45fc0967fb1d1e84bf7c61
Devendra Singh <dev.shekhawat.dev@gmail.com>
2017-04-29 21:17:05 +0530

    rg

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $cURL = curl_init();
 curl_setopt($cURL, CURLOPT_URL, $url);
 curl_setopt($cURL, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 
-
 $result = curl_exec($cURL);
-
+$j = json_decode($result,[TRUE,[int depth=512]]);
+echo $j['botsay'];
 ?>
