6de0ebb60e4f99bac926545f197de6fe45c75b30
Devendra Singh Shekhawat <dev.shekhawat.dev@gmail.com>
2017-04-29 21:50:16 +0530

    Update test.php

--- test.php
+++ test.php
@@ -2,12 +2,7 @@
 
 $url ="http://api.program-o.com/v2/chatbot/?bot_id=6&say=what%20is%20your%20name&convo_id=exampleusage_1231232&format=json";
 
-$cURL = curl_init();
-
-curl_setopt($cURL, CURLOPT_URL, $url);
-curl_setopt($cURL, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
-$result = curl_exec($cURL);
+$result = file_get_contents($url);
 $j =json_decode($result);
 echo $j->{'botsay'};
 ?>
