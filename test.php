592e62138a32fc8d07d8029b8bf27c674fd1955d
Devendra Singh Shekhawat <dev.shekhawat.dev@gmail.com>
2017-04-29 10:36:42 +0530

    Update test.php

--- test.php
+++ test.php
@@ -6,12 +6,8 @@ $url ="http://api.program-o.com/v2/chatbot/?bot_id=6&say=what%20is%20your%20name
 $cURL = curl_init();
 
 curl_setopt($cURL, CURLOPT_URL, $url);
-curl_setopt($cURL, CURLOPT_HTTPGET, true);
+curl_setopt($cURL, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 
-curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Accept: application/json'
-));
 
 $result = curl_exec($cURL);
 //echo $result;
